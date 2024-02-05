af62d794bbaf9a0b0a28b9eea433e2f044dea328
EvildoerXiaoyy <xiaoyyprocess@163.com>
2024-02-05 23:29:17 +0800

    feature: samuel的docker学习之旅-namespace

---
+++ main.go
@@ -0,0 +1,27 @@
+//go:build linux
+
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main() {
+	cmd := exec.Command("sh")
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID |
+			syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER | syscall.CLONE_NEWNET,
+	}
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	os.Stderr = os.Stderr
+
+	err := cmd.Run
+	if err != nil {
+		log.Fatal(err())
+	}
+	os.Exit(-1)
+}

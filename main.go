d0b1f27872b0d37261c1e4986c7fc76201ad6d9e
EvildoerXiaoyy <xiaoyyprocess@163.com>
2024-02-05 23:38:12 +0800

    feature: 添加注释

--- main.go
+++ main.go
@@ -11,6 +11,13 @@ import (
 
 func main() {
 	cmd := exec.Command("sh")
+	// 7种namespace作用
+	// CLONE_NEWUTS: new nodename domainname namespace
+	// CLONE_NEWIPC: new ipc资源 namespace
+	// CLONE_NEWPID: new pid 视图 namespace
+	// CLONE_NEWNS: new mount namespace
+	// CLONE_NEWUSER: new user namespace
+	// CLONE_NEWNET: new network namespace
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID |
 			syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER | syscall.CLONE_NEWNET,
